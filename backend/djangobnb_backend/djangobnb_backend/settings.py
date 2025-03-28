b8230206224b12839e8e1cc6c7f8761160dcf3ba
Jesus Trinidad <trinidadjesus117@gmail.com>
2025-03-28 17:44:18 -0600

    Get properties in the front-end

--- backend/djangobnb_backend/djangobnb_backend/settings.py
+++ backend/djangobnb_backend/djangobnb_backend/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import os
 from pathlib import Path
 
@@ -18,6 +19,48 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 AUTH_USER_MODEL = 'useraccount.User'
 
+SITE_ID= 1
+
+WEBSITE_URL='http://localhost:8000'
+
+SIMPLE_JWT= {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKEN": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "SIGNING_KEY": "acomplexkey",
+    "ALOGRIGTHM":"H5512",
+}
+
+ACCOUNT_USER_MODEL_USERNAME_FIELD= None
+ACCOUNT_EMAIL_REQUIRED= True
+ACCOUNT_USERNAME_REQUIRED=False
+ACCOUNT_AUTHENTICATION_METHOD='email'
+ACCOUNT_EMAIL_VERIFICAYION = None
+
+RES_FRAMEWORK={
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'res_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+REST_AUTH={
+    "USE_JWT":True,
+    "JWT_AUTH_HTTPONLY":False
+}
+
+
+
 
 # Application definition
 
@@ -28,12 +71,27 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+
+    'allauth',
+    'allauth.account',
+
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+
+    'corsheaders',
+
     'useraccount',
+    'property',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -41,7 +99,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangobnb_bakcend.urls'
+ROOT_URLCONF = 'djangobnb_backend.urls'
 
 TEMPLATES = [
     {
@@ -59,7 +117,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'djangobnb_bakcend.wsgi.application'
+WSGI_APPLICATION = 'djangobnb_backend.wsgi.application'
 
 
 # Database
@@ -112,6 +170,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL= '/media/'
+MEDIA_ROOT= BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- backend/djangobnb_backend/djangobnb_backend/wsgi.py
+++ backend/djangobnb_backend/djangobnb_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobnb_bakcend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobnb_backend.settings')
 
 application = get_wsgi_application()
