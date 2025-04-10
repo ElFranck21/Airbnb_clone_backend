987771382335dcd541430b12e66120c0f67af6c2
Jesus Trinidad <trinidadjesus117@gmail.com>
2025-04-09 21:05:12 -0600

    Reservations model

--- backend/djangobnb_backend/property/serializers.py
+++ backend/djangobnb_backend/property/serializers.py
@@ -1,12 +1,29 @@
+from useraccount.serializers import UserDetailSerializer
 from rest_framework import serializers
 from .models import Property
 
 class PropertiesListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
-        fields=[
+        fields=(
             'id',
             'title',
             'price_per_night',
             'image_url'
-        ]
+        )
+
+class PropertiesDetailSerializer(serializers.ModelSerializer):
+    landlord = UserDetailSerializer(read_only=True, many=False)
+    class Meta:
+        model = Property
+        fields=(
+            'id',
+            'title',
+            'description',
+            'price_per_night',
+            'image_url',
+            'bedrooms',
+            'bathrooms',
+            'guests',
+            'landlord'
+        )
---
+++ backend/djangobnb_backend/useraccount/serializers.py
@@ -0,0 +1,13 @@
+from property.models import Property
+from rest_framework import serializers
+
+from .models import User
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= User
+        fields=(
+            'id',
+            'name',
+            'avatar_url',
+        )
